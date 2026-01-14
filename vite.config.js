b9f126b01c71280d86e9362f4ebe7216b18fced9
adnancholayil <adnancholayil123@gmail.com>
2026-01-14 11:32:01 +0530

    fix: Fix build error by removing experimental babel compiler

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", { target: "18" }],
-        ],
-      },
-    }),
+    react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
